4e9bad1ea8cdd1dcf4cac968daaa21b4a86eaa71
Adithya Hegde <adithyah07@gmail.com>
2022-02-02 21:46:23 +0530

    ~

--- cmp2strings/strcmp.asm
+++ cmp2strings/strcmp.asm
@@ -11,12 +11,13 @@ str2 db 100h dup(?)
 data ends  
 
 code segment
-assume cs:code,ds:data 
+assume cs:code,ds:data,es:data 
 
 start:
 ;init 
 mov ax,data 
 mov ds,ax 
+mov es,ax
 
 ;read str1 
 
@@ -24,49 +25,49 @@ lea si,str1
 mov cx,00h
 display msg1
 read1:
-read 
-mov [si],al 
-inc si 
-inc cx
-cmp al,0dh 
-jnz read1
+  read 
+  mov [si],al 
+  inc si 
+  inc cx
+  cmp al,0dh 
+  jnz read1
 
 lea di,str2
 mov bx,00h
 display msg2
 read2:
-read 
-mov [di],al 
-inc di 
-inc bx
-cmp al,0dh 
-jnz read2 
+  read 
+  mov [di],al 
+  inc di 
+  inc bx
+  cmp al,0dh 
+  jnz read2 
 
 lea si,str1 
 lea di,str2
 
 cmp bx,cx 
 jnz noteql
+
 comp_loop:
-mov al,[si] 
-mov bl,[di] 
-cmp al,bl 
-jnz noteql
-inc si 
-inc di 
-dec cx 
-cmp cx,0 
-jnz comp_loop
+  mov al,[si] 
+  mov bl,[di] 
+  cmp al,bl 
+  jnz noteql
+  inc si 
+  inc di 
+  loop comp_loop
 
 display msg3 
 jmp exit
-noteql:
 
-display msg4 
+noteql:
+  display msg4 
 
 exit:
-mov ah,4ch
-int 21h
+  mov ah,4ch
+  int 21h
+
 code ends 
 end start
 
